13ecf258cb2d78c1cc9ba1e0089b6143e5fb3d35
Malcolm Leake <echelon1089@gmail.com>
2018-05-20 01:15:51 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,13 @@
+
+  def instantiate_new_array 
+    array = []
+  end 
+  
+  def array_with_two_elements
+    building = ["park", "slope"]
+  end
+  
+  def first_element(building)
+    building = [ "park", "slope"]
+    building[0]
+  end
